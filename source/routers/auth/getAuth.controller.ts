1a9b52f34de864a35b9a200632693fb3be4429bb
H2Owater425 <h2o@h2owr.xyz>
2022-10-03 07:53:03 +0900

    feat: Make reply adjust to serialization

--- source/routers/auth/getAuth.controller.ts
+++ source/routers/auth/getAuth.controller.ts
@@ -2,13 +2,8 @@ import { FastifyRequest, PayloadReply } from 'fastify'
 
 export default (request: FastifyRequest, reply: PayloadReply) => {
   reply.send({
-    status: 'success',
-    data: [
-      {
-        title: 'Auth',
-        body: 'User auth module',
-      },
-    ],
+    title: 'Auth',
+    body: 'User auth module',
   })
 
   return
--- source/routers/getRoot.controller.ts
+++ source/routers/getRoot.controller.ts
@@ -2,12 +2,7 @@ import { FastifyRequest, PayloadReply } from 'fastify'
 
 export default (request: FastifyRequest, reply: PayloadReply) => {
   reply.send({
-    status: 'success',
-    data: [
-      {
-        message: 'glint',
-      },
-    ],
+    message: 'glint',
   })
 
   return
