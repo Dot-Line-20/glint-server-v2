a4cc28a983803fd8e836f16b33519bf9a9dee3a9
H2Owater425 <h2o@h2owr.xyz>
2022-10-11 19:40:01 +0900

    feat: Move existence checker to prisma.ts

--- source/library/prisma.ts
+++ source/library/prisma.ts
@@ -1,3 +1,57 @@
 import { PrismaClient } from '@prisma/client'
 
-export default new PrismaClient()
+export const prisma: PrismaClient = new PrismaClient()
+
+export async function isUserExists(id: number): Promise<boolean> {
+  return (
+    (await prisma.user.findFirst({
+      where: {
+        id: id,
+        verificationKey: null,
+      },
+    })) !== null
+  )
+}
+
+export async function isScheduleExists(
+  userId: number,
+  id: number
+): Promise<boolean> {
+  return (
+    (await prisma.schedule.findFirst({
+      where: {
+        id: id,
+        userId: userId,
+      },
+    })) !== null
+  )
+}
+
+export async function isPostExists(id: number): Promise<boolean> {
+  return (
+    (await prisma.post.findFirst({
+      where: {
+        id: id,
+        isDeleted: false,
+      },
+    })) !== null
+  )
+}
+
+export async function isCommentExists(
+  postId: number,
+  id: number
+): Promise<boolean> {
+  return (
+    (await prisma.comment.findFirst({
+      where: {
+        id: id,
+        post: {
+          id: postId,
+          isDeleted: false,
+        },
+        isDeleted: false,
+      },
+    })) !== null
+  )
+}
