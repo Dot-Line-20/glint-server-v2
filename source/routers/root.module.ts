402686fb0b8f9d07bc92e666de78b30a7b2ea33e
H2Owater425 <h2o@h2owr.xyz>
2022-10-11 19:42:36 +0900

    feat: Add module

--- source/routers/root.module.ts
+++ source/routers/root.module.ts
@@ -1,6 +1,7 @@
 import Module from '@library/module'
 import authModule from './auth/auth.module'
 import getRootController from './getRoot.controller'
+import postsModule from './posts/posts.module'
 import usersModule from './users/users.module'
 
 export default new Module({
@@ -11,6 +12,6 @@ export default new Module({
       handler: getRootController,
     },
   ],
-  modules: [authModule, usersModule],
+  modules: [authModule, postsModule, usersModule],
   prefix: '',
 })
