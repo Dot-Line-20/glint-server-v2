8a6e2a8309843c0808a50627a9b247e0a6c033b5
H2Owater425 <h2o@h2owr.xyz>
2022-10-29 22:04:40 +0900

    feat: Make response contains categories

--- source/routers/posts/getPost.controller.ts
+++ source/routers/posts/getPost.controller.ts
@@ -38,6 +38,11 @@ export default async (
           media: true,
         },
       },
+      categories: {
+        select: {
+          category: true,
+        },
+      },
       likes: {
         where: {
           userId: request.userId,
